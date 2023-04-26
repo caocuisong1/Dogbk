a04186010f622c2ea2bedb6938885518075bd9e9
caocuisong1 <caocuisong0823@163.com>
2023-04-26 18:15:54 +0800

    5

--- myScript.js
+++ myScript.js
@@ -1,1 +1,11 @@
 window.alert('欢迎来到犬百科');
+function myFunction()
+{
+	x=document.getElementById("demo");  // 找到元素
+	x.innerHTML="Hello JavaScript!";    // 改变内容
+}
+function myBackground()
+{
+x=document.getElementById("chbk");
+x.style.background="#292421";
+}
